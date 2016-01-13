f6579023faf262b8c09ab4acab53d3a226437cd1
vidaaudrey <audrey@33road.com>
2016-01-12 19:28:21 -0800

    test: add testing lib, script and mockup

---
+++ src/index.spec.js
@@ -0,0 +1,9 @@
+"use strict";
+const expect = require('chai').expect
+const helloLib = require('./index')
+
+describe('hello-international lib', function () {
+  it('should work', function () {
+    expect(true).to.be.true
+  });
+});
